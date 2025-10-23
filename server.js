7ef00666baa8cefcedba848d77425f5f2d38145e
Vivek5709 <166849822+Vivek5709@users.noreply.github.com>
2025-10-23 18:33:48 +0530

    Replace mysql with mysql2 in server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mysql = require("mysql");
+const mysql2 = require("mysql2");
 const path = require("path");
 const bodyParser = require("body-parser");
 require("dotenv").config();
